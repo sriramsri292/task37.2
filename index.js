c8ad8bc7d827dd75671a21a6f66ae5d428897967
“sriramsri@292” <sriram27435@gmail.com>
2023-12-20 14:32:38 +0530

    Initial commit

--- index.js
+++ index.js
@@ -2,39 +2,42 @@ const express = require("express");
 const http_server = express();
 const port = 5000;
 const fs = require('fs').promises;
+const moment = require('moment'); // Add this line to use the moment library
 
-const content = new Date().toString();
+const folderPath = './files/'; // Use a descriptive folder name
 
 async function createFile() {
   try {
-    await fs.writeFile('./files/test.txt', content);
-    console.log('File written successfully');
+    // Ensure the folder exists
+    await fs.mkdir(folderPath, { recursive: true });
 
-    try {
-      const data = await fs.readFile('./files/test.txt', 'utf8');
-      console.log(data);
-    } catch (err) {
-      console.error(err);
-    }
+    // Generate the filename with current date and time
+    const fileName = moment().format('YYYY-MM-DD HH-mm-ss') + '.txt';
+
+    // Create the file with the current timestamp
+    await fs.writeFile(folderPath + fileName, new Date().toString());
+    console.log('File written successfully:', fileName);
+
   } catch (err) {
-    console.error(err);
+    console.error('Error:', err);
   }
 }
 
-
+// Create a specific endpoint for creating the text file
+http_server.post('/createFile', async (req, res) => {
+  try {
+    await createFile();
+    return res.status(200).json({
+      message: "File created successfully",
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+});
 
 http_server.listen(port, 'localhost', () => {
     console.log("Server is running on port " + port);
-});
-
-http_server.use('/', (req, res, next) => {
-    console.log("hai bro");
-    createFile();
-    return res.status(200).json({
-        message:" hellow buddy",
-        number:9848,
-        number:6,
-        
-    })
-  
 });
